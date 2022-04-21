66477d4d11e90f4ac5c56fe11baf17b7f3eb8c4b
Gavin Loo <looweiren@gmail.com>
2022-04-22 02:36:08 +0800

    styled remaining components

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function About() {
     return (
-        <section>
+        <section className="about">
             <h1>About</h1>
             <p>Insert some random text here, Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters tall. Japan is the best country ever, I love eating Sushi especially Salmon Sashimi. The fatty ones are the best.</p>
         </section>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { FaTwitterSquare, FaFacebookSquare, FaInstagramSquare, FaGithubSquare } from "react-icons/fa"
+
+// Twitter, Facebook, IG, Github
 
 export default function Footer() {
+
+    const iconStyle = { fontSize: "2em", marginLeft: "7px", marginRight: "7px", color:"rgb(148, 148, 148)"}
+
     return (
-        <h3>Some footer text here.</h3>
+        <footer className="footer">
+            <FaTwitterSquare style={iconStyle}/>
+            <FaFacebookSquare style={iconStyle}/>
+            <FaInstagramSquare style={iconStyle}/>
+            <FaGithubSquare style={iconStyle}/>
+        </footer>
     )
 }
