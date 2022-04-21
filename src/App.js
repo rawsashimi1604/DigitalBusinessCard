a5ca2be9c1d1fa0e8069cee214e564c38665313a
Gavin Loo <looweiren@gmail.com>
2022-04-22 01:52:31 +0800

    initial startup for repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Info from "./components/Info"
+import About from "./components/About"
+import Footer from "./components/Footer"
+import Interests from "./components/Interests"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./style.css"
+
+
+export default function App() {
+    return (
+        <h1>MyApp</h1>
+    )
 }
-
-export default App;
